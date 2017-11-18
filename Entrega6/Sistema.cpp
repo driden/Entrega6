72ae1f03e1a225d913009f28303fa2e6d3196c9c
driden <driden_87@hotmail.com>
2017-11-18 16:11:15 -0300

    Interfaz

---
+++ Entrega6/Sistema.cpp
@@ -0,0 +1,83 @@
+﻿#include "Sistema.h"
+
+Sistema::Sistema()
+{
+
+}
+
+  Iterador<Iterador<Puntero<ICiudad>>> Sistema::Viajero(Array<Puntero<ICiudad>> &ciudadesDelMapa, Matriz<nat> &mapa, Puntero<ICiudad> &ciudadPartida, Iterador<Puntero<ICiudad>> &ciudadesPasar, nat costoMax) 
+ {
+	 /*
+	Codigo de implementacion de la solucion
+	*/
+	 return NULL; //Retorno por defecto
+ }
+	
+ Array<nat> Sistema::Intercalar(Array<nat> &arreglo, nat i, nat m, nat d) 
+ {
+	 /*
+	Codigo de implementacion de la solucion
+	*/
+	 return Array<nat>(arreglo.Largo);  //Retorno por defecto
+ }
+
+
+Iterador<Tupla<nat,nat>> Sistema::Laberinto(Tupla<nat,nat> &inicio, Tupla<nat,nat> &fin, Matriz<nat> &laberinto)
+{
+	/*
+	Codigo de implementacion de la solucion
+	*/
+	return NULL;  //Retorno por defecto
+}
+
+Array<nat> Sistema::Degustacion(Array<Producto> productos, nat maxDinero, nat maxCalorias, nat maxAlcohol)
+{
+	/*
+	Codigo de implementacion de la solucion
+	*/
+	 return Array<nat>(productos.Largo);  //Retorno por defecto
+}
+
+Tupla<TipoRetorno, Iterador<nat>> Sistema::Viajero2(Matriz<Tupla<nat, nat, nat>> relacionesCiudades, Iterador<nat> CiudadesPasar, Iterador<nat> CiudadesNoPasar)
+{
+	//Implementar.
+
+	return Tupla<TipoRetorno, Iterador<nat>>();
+}
+
+Tupla<TipoRetorno,  Array<nat>> Sistema::ProteccionAnimales(Array<Accion> acciones, nat maxVeterinarios, nat maxVehiculos, nat maxDinero, nat maxVacunas, nat maxVoluntarios)
+{
+	//Implementar.
+
+	return Tupla<TipoRetorno,  Array<nat>>();
+}
+
+Array<nat> Sistema::QuickSort(Array<nat> elementos)
+{
+	//Implementar.
+
+	return Array<nat>();
+}
+
+Tupla<TipoRetorno, Iterador<Iterador<Tupla<int, int>>>> Sistema::CaminoCaballo(Tupla<int, int>& salida, Tupla<int, int>& destino, nat cantAPasar, nat tamTablero, Iterador<Tupla<int, int>>& pasar, Iterador<Tupla<int, int>>& noPasar)
+{
+	//Implementar.
+
+	return Tupla<TipoRetorno, Iterador<Iterador<Tupla<int, int>>>>();
+}
+
+Tupla<TipoRetorno, Array<nat>> Sistema::OptimizarGranja(Array<Tupla<nat, nat, nat>>& semillas, nat dinero, nat tierra, nat agua)
+{
+	//Implementar.
+
+	return Tupla<TipoRetorno, Array<nat>>();
+}
+
+Tupla<TipoRetorno, Iterador<Tupla<Cadena, bool>>> Sistema::InscribirMaterias(Iterador<Tupla<Cadena, nat, nat>> matutino, Iterador<Tupla<Cadena, nat, nat>> nocturno, nat horasM, nat horasN)
+{
+	//Implementar.
+
+	return Tupla<TipoRetorno, Iterador<Tupla<Cadena, bool>>>();
+}
+
+
---
+++ Entrega6/Sistema.h
@@ -0,0 +1,45 @@
+﻿#ifndef SISTEMA_H
+#define SISTEMA_H
+
+#include "ISistema.h"
+#include "Producto.h"
+
+class Sistema : public ISistema
+{
+public:
+	Sistema();
+
+	Iterador<Tupla<nat,nat>> Laberinto(Tupla<nat,nat> &inicio, Tupla<nat,nat> &fin, Matriz<nat> &laberinto) override;
+
+	virtual Iterador<Iterador<Puntero<ICiudad>>> Viajero(Array<Puntero<ICiudad>> &ciudadesDelMapa, Matriz<nat> &mapa, Puntero<ICiudad> &ciudadPartida, Iterador<Puntero<ICiudad>> &ciudadesPasar, nat costoMax) override;
+	
+	virtual Array<nat> Intercalar(Array<nat> &arreglo, nat i, nat m, nat d) override;
+
+	virtual Array<nat> Degustacion(Array<Producto> productos, nat maxDinero, nat maxCalorias, nat maxAlcohol) override;
+
+	//PRE: relacionesCiudades representa la matriz de adyacencia. Para un par de ciudades i,j relacionCiudades[i][j] devuelve una tupla con el costo, distancia y dinero (en ese orden) necesarios para viajar desde i hasta j.
+	//POS: se retorna OK y un iterador con las ciudades a recorrer en orden.
+	virtual Tupla<TipoRetorno, Iterador<nat>> Viajero2(Matriz<Tupla<nat, nat, nat>> relacionesCiudades, Iterador<nat> CiudadesPasar, Iterador<nat> CiudadesNoPasar) override;
+
+	//PRE: recibe una array con todas las acciones posibles, indicando los recursos que se necesitan para cada accion y el impacto que tiene.
+	//POST: retorna OK y un array de booleanos indicando cuántas veces realizar cada acción para maximizar el impacto.
+	virtual Tupla<TipoRetorno,  Array<nat>> ProteccionAnimales(Array<Accion> acciones, nat maxVeterinarios, nat maxVehiculos, nat maxDinero, nat maxVacunas, nat maxVoluntarios) override;
+
+	//POST: Ordena el array usando la tecnica de quicksort.
+	virtual Array<nat> QuickSort(Array<nat> elementos) override;
+
+	//PRE: pasar y noPasar son iteradores de casillas validas
+	//POS: OK si existe camino. Iterador de mejores camino (cada camino se representa con un Iterador de casillas o Tuplas<int, int>)
+	virtual Tupla<TipoRetorno, Iterador<Iterador<Tupla<int, int>>>> CaminoCaballo(Tupla<int, int>& salida, Tupla<int, int>& destino, nat cantAPasar, nat tamTablero, Iterador<Tupla<int, int>>& pasar, Iterador<Tupla<int, int>>& noPasar) override;
+
+	//PRE: semillas es un Array que contiene una Tupla de <costo siembra, agua necesaria, ganancia>
+	//POS: se retorna OK, y un array indicando cantidad de hectáreas por especie.
+	virtual Tupla<TipoRetorno, Array<nat>> OptimizarGranja(Array<Tupla<nat, nat, nat>>& semillas, nat dinero, nat tierra, nat agua) override;
+
+	//PRE: matutino y nocturno representan un Iterador de Tupla<materia, creditos, horas>
+	//POS: OK si existe solución y un iterador de Tupla<materia, es de turno matutino>
+	virtual Tupla<TipoRetorno, Iterador<Tupla<Cadena, bool>>> InscribirMaterias(Iterador<Tupla<Cadena, nat, nat>> matutino, Iterador<Tupla<Cadena, nat, nat>> nocturno, nat horasM, nat horasN) override;
+private:
+};
+
+#endif
